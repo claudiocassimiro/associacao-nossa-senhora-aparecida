aa2faf1993dfd20511b6928fa6c8e87a2eafef1e
claudiocassimiro <days_surfing@live.com>
2022-11-23 15:27:49 -0300

    feat: implements infos

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,10 @@ import AboutEvent from "../components/AboutEvent";
 import SwiperBingoPrize from "../components/SwiperBingoPrize";
 import BuyYourCardButton from "../components/BuyYourCardButton";
 import Head from "next/head";
+import { GiPartyPopper } from "react-icons/gi";
+import { GrStatusGood } from "react-icons/gr";
+import { FaChild, FaPhoneAlt } from "react-icons/fa";
+import { HiLocationMarker } from "react-icons/hi";
 
 export default function Home() {
   const { isDesktop } = useWindowSize();
@@ -41,11 +45,46 @@ export default function Home() {
         <BuyYourCardButton />
         <SwiperBingoPrize />
         <BuyYourCardButton />
+
+        <div className={styles.HomeContainerMoreInfos}>
+          <Image
+            src="/images/logo-associacao-768x282.png"
+            alt="logo da associação"
+            width={260}
+            height={120}
+          />
+          <div className={styles.HomeContainerInfos}>
+            <span className={styles.HomeContainerIconAndText}>
+              <GiPartyPopper size="20" />
+              <p className={styles.HomeInfoText}>
+                Venha fazer parte da nossa festa!
+              </p>
+            </span>
+            <span className={styles.HomeContainerIconAndText}>
+              <GrStatusGood size="20" />
+              <p className={styles.HomeInfoText}>Bingo Beneficente</p>
+            </span>
+            <span className={styles.HomeContainerIconAndText}>
+              <FaChild size="20" />
+              <p className={styles.HomeInfoText}>
+                Festa das Crianças e Natal Solidário
+              </p>
+            </span>
+            <span className={styles.HomeContainerIconAndText}>
+              <HiLocationMarker size="20" />
+              <p className={styles.HomeInfoText}>Chácara do Gaúcho</p>
+            </span>
+            <span className={styles.HomeContainerIconAndText}>
+              <FaPhoneAlt size="15" />
+              <p className={styles.HomeInfoText}>(81) 81- 99177-5580</p>
+            </span>
+          </div>
+        </div>
       </main>
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <h2>Olá footer</h2>
-      </footer>
+      </footer> */}
     </div>
   );
 }
