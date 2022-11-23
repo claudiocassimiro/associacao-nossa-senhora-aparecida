48e838acd7f7f6494959b147b9a374775daa634a
claudiocassimiro <days_surfing@live.com>
2022-11-23 11:06:46 -0300

    feat: implements SwiperBingoPrize component

---
+++ components/SwiperBingoPrize/index.tsx
@@ -0,0 +1,64 @@
+import styles from "./styles.module.css";
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import { handleImageSize } from "../../utils/helpers";
+
+export default function SwiperBingoPrize() {
+  return (
+    <div className={styles.SwiperBingoPrizeContainer}>
+      <h2 className={styles.SwiperBingoPrizeTitle}>Prêmio do Bingo</h2>
+      <div className={styles.SwiperSwiperContainer}>
+        <Swiper
+          navigation={true}
+          modules={[Navigation]}
+          className={styles.SwiperSwiper}
+        >
+          <SwiperSlide className={styles.SwiperSwiperSlide}>
+            <Image
+              src="/images/imagem-premio-1.jpeg"
+              alt="Imagens do premio do bingo"
+              width={handleImageSize().width}
+              height={handleImageSize().height}
+            />
+          </SwiperSlide>
+          <SwiperSlide className={styles.SwiperSwiperSlide}>
+            <Image
+              src="/images/imagem-premio-2.jpeg"
+              alt="Imagens do premio do bingo"
+              width={handleImageSize().width}
+              height={handleImageSize().height}
+            />
+          </SwiperSlide>
+          <SwiperSlide className={styles.SwiperSwiperSlide}>
+            <Image
+              src="/images/imagem-premio-3.jpeg"
+              alt="Imagens do premio do bingo"
+              width={handleImageSize().width}
+              height={handleImageSize().height}
+            />
+          </SwiperSlide>
+          <SwiperSlide className={styles.SwiperSwiperSlide}>
+            <Image
+              src="/images/imagem-premio-4.jpeg"
+              alt="Imagens do premio do bingo"
+              width={handleImageSize().width}
+              height={handleImageSize().height}
+            />
+          </SwiperSlide>
+          <SwiperSlide className={styles.SwiperSwiperSlide}>
+            <Image
+              src="/images/imagem-premio-5.jpeg"
+              alt="Imagens do premio do bingo"
+              width={handleImageSize().width}
+              height={handleImageSize().height}
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+}
