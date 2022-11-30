ef76d2cdb5ec659370c01e767b4ef999cc4118b9
claudiocassimiro <days_surfing@live.com>
2022-11-30 15:35:16 -0300

    feat: implements footer component

---
+++ components/Footer/index.tsx
@@ -0,0 +1,85 @@
+import styles from "./styles.module.css";
+import {
+  AiOutlineInstagram,
+  AiOutlineWhatsApp,
+  AiOutlineFacebook,
+  AiOutlineYoutube,
+} from "react-icons/ai";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+export default function Footer() {
+  const router = useRouter();
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.containerImageAndLinkElements}>
+        <Image
+          src="/images/logo-associacao-768x282.png"
+          alt="logo da associação"
+          width={120}
+          height={50}
+        />
+        <div className={styles.containerLinkElements}>
+          <a
+            className={styles.textLink}
+            onClick={() => router.push("/about-us")}
+          >
+            Quem Somos
+          </a>
+          <a
+            className={styles.textLink}
+            onClick={() => router.push("/about-us")}
+          >
+            Regulamento
+          </a>
+        </div>
+      </div>
+      <div className={styles.containerSocialLinks}>
+        <div className={styles.socialContainer}>
+          <AiOutlineInstagram size="20" />
+          <a
+            className={styles.socialLink}
+            href="https://www.instagram.com/bola10solidario/?igshid=YmMyMTA2M2Y%3D"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Instagram
+          </a>
+        </div>
+        <div className={styles.socialContainer}>
+          <AiOutlineWhatsApp size="20" />
+          <a
+            className={styles.socialLink}
+            href="https://www.instagram.com/bola10solidario/?igshid=YmMyMTA2M2Y%3D"
+            target="_blank"
+            rel="noreferrer"
+          >
+            WhatsApp
+          </a>
+        </div>
+        <div className={styles.socialContainer}>
+          <AiOutlineFacebook size="20" />
+          <a
+            className={styles.socialLink}
+            href="https://www.instagram.com/bola10solidario/?igshid=YmMyMTA2M2Y%3D"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Facebook
+          </a>
+        </div>
+        <div className={styles.socialContainer}>
+          <AiOutlineYoutube size="20" />
+          <a
+            className={styles.socialLink}
+            href="https://www.instagram.com/bola10solidario/?igshid=YmMyMTA2M2Y%3D"
+            target="_blank"
+            rel="noreferrer"
+          >
+            YouTube
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
