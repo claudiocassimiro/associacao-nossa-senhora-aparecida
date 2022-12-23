222daa975673ded3bbee5c43782b2a426b3a20e4
claudiocassimiro <days_surfing@live.com>
2022-12-23 11:41:17 -0300

    feat: implements menu with some itens

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,19 @@ import { GiPartyPopper } from "react-icons/gi";
 import { GrStatusGood } from "react-icons/gr";
 import { FaChild, FaPhoneAlt } from "react-icons/fa";
 import { HiLocationMarker } from "react-icons/hi";
+import { GiHamburgerMenu } from "react-icons/gi";
 import Footer from "../components/Footer";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineWhatsApp } from "react-icons/ai";
+import { AiOutlineFacebook } from "react-icons/ai";
+import { AiOutlineYoutube } from "react-icons/ai";
 
 export default function Home() {
   const { isDesktop } = useWindowSize();
+  const router = useRouter();
+  const [showMenu, setShowMenu] = useState(false);
 
   return (
     <div className={styles.container}>
@@ -33,7 +42,64 @@ export default function Home() {
             width={isDesktop ? 768 : 300}
             height={isDesktop ? 282 : 143}
           />
+          <GiHamburgerMenu
+            onClick={() => setShowMenu(!showMenu)}
+            size={32}
+            className={styles.HomeMenuButton}
+          />
         </div>
+        {showMenu ? (
+          <div className={styles.HomeMenu}>
+            <a
+              className={styles.HomeMenuItem}
+              onClick={() => router.push("/about-us")}
+            >
+              Quem Somos
+            </a>
+            <a
+              className={styles.HomeMenuItem}
+              onClick={() => router.push("/regulation")}
+            >
+              Regulamento
+            </a>
+            <a
+              href="https://www.instagram.com/bola10solidario/?igshid=YmMyMTA2M2Y%3D"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.HomeMenuItem}
+            >
+              <AiOutlineInstagram size="20" />
+              <p className={styles.socialLink}>Instagram</p>
+            </a>
+            <a
+              href={`https://api.whatsapp.com/send?phone=5581974002486&text=Ol%c3%a1%2c%20gostaria%20de%20informa%c3%a7%c3%b5es%20a%20respeito%20do%20bingo!`}
+              target="_blank"
+              rel="noreferrer"
+              className={styles.HomeMenuItem}
+            >
+              <AiOutlineWhatsApp size="20" />
+              <p className={styles.socialLink}>WhatsApp</p>
+            </a>
+            <a
+              href="https://pt-br.facebook.com/people/Associa%C3%A7%C3%A3o-Nossa-Senhora-Aparecida/100085310482679/"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.HomeMenuItem}
+            >
+              <AiOutlineFacebook size="20" />
+              <p className={styles.socialLink}>Facebook</p>
+            </a>
+            <a
+              href="https://www.youtube.com/channel/UCNxEoY00hlrtkmezPDqMJxw"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.HomeMenuItem}
+            >
+              <AiOutlineYoutube size="20" />
+              <p className={styles.socialLink}>YouTube</p>
+            </a>
+          </div>
+        ) : null}
         <div className={styles.HomeHeroContainer}>
           <h1 className={styles.HomeHeroTitle}>
             Venha Participar do Nosso Bingo Beneficente!
